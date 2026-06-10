95d7b46b2389f8921f54811f573d8910c8090bfa
GabrielDSousa <gabrielramos.email@gmail.com>
2026-06-10 18:31:15 -0300

    feat: Implement Payment model, migrations, and database tests

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -13,37 +13,40 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
+        'country',
+        'currency_code',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function paymentRequests(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    public function pendingPayments(): HasMany
+    {
+        return $this->hasMany(Payment::class, 'pending');
+    }
+
+    public function approvedPayments(): HasMany
+    {
+        return $this->hasMany(Payment::class, 'approved_by');
     }
 }
