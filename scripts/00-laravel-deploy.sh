af60bc7b7fc939da89594dba521cb4f35cb9c06b
GabrielDSousa <gabrielramos.email@gmail.com>
2026-06-14 12:01:19 -0300

    feat: update composer install command to include optimize autoloader option

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+composer install --no-dev --optimize-autoloader
 
 echo "Prepare the application ..."
 php artisan key:generate
